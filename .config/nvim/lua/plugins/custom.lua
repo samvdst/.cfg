282659d2243bee04eb151a9aa22fab0c62f45c65
Samuel Van der Stappen <sam@vd.st>
2024-02-12 14:22:41 +0100

    updates

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -24,24 +24,8 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "prisma",
-        "rust",
-      },
+      ensure_installed = "all",
+      ignore_install = { "bicep", "jsdoc", "groovy" },
     },
   },
 
