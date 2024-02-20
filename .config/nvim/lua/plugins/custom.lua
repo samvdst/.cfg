e7bd12c5190262a6a012da8bf44804e7f3c158c7
Samuel Van der Stappen <sam@vd.st>
2024-02-20 14:58:53 +0100

    update i3 and nvim

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -7,6 +7,13 @@ return {
     end,
   },
 
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+
   -- wakatime plugin
   {
     "wakatime/vim-wakatime",
@@ -25,7 +32,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = "all",
-      ignore_install = { "bicep", "jsdoc", "groovy" },
+      ignore_install = { "bicep", "jsdoc", "groovy", "slint" },
     },
   },
 
@@ -46,38 +53,38 @@ return {
     },
   },
 
-  {
-    "nvim-lualine/lualine.nvim",
-    optional = true,
-    event = "VeryLazy",
-    opts = function(_, opts)
-      local Util = require("lazyvim.util")
-      local colors = {
-        [""] = Util.ui.fg("Special"),
-        ["Normal"] = Util.ui.fg("Special"),
-        ["Warning"] = Util.ui.fg("DiagnosticError"),
-        ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
-      }
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          local icon = require("lazyvim.config").icons.kinds.Copilot
-          local status = require("copilot.api").status.data
-          return icon .. (status.message or "")
-        end,
-        cond = function()
-          local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
-          return ok and #clients > 0
-        end,
-        color = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local status = require("copilot.api").status.data
-          return colors[status.status] or colors[""]
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   optional = true,
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     local Util = require("lazyvim.util")
+  --     local colors = {
+  --       [""] = Util.ui.fg("Special"),
+  --       ["Normal"] = Util.ui.fg("Special"),
+  --       ["Warning"] = Util.ui.fg("DiagnosticError"),
+  --       ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
+  --     }
+  --     table.insert(opts.sections.lualine_x, 2, {
+  --       function()
+  --         local icon = require("lazyvim.config").icons.kinds.Copilot
+  --         local status = require("copilot.api").status.data
+  --         return icon .. (status.message or "")
+  --       end,
+  --       cond = function()
+  --         local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+  --         return ok and #clients > 0
+  --       end,
+  --       color = function()
+  --         if not package.loaded["copilot"] then
+  --           return
+  --         end
+  --         local status = require("copilot.api").status.data
+  --         return colors[status.status] or colors[""]
+  --       end,
+  --     })
+  --   end,
+  -- },
 
   -- -- sourcegraph cody (alternative to copilot)
   -- {
@@ -91,56 +98,56 @@ return {
   --   end,
   -- },
 
-  -- GitHub Copilot
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<A-l>",
-          prev = "<A-[>",
-          next = "<A-]>",
-          dismiss = "<C-]>",
-        },
-      },
-      panel = {
-        auto_refresh = false,
-        keymap = {
-          accept = "<CR>",
-          jump_prev = "[[",
-          jump_next = "]]",
-          refresh = "gr",
-          open = "<A-CR>",
-        },
-      },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
+  -- -- GitHub Copilot
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = {
+  --       auto_trigger = true,
+  --       keymap = {
+  --         accept = "<A-l>",
+  --         prev = "<A-[>",
+  --         next = "<A-]>",
+  --         dismiss = "<C-]>",
+  --       },
+  --     },
+  --     panel = {
+  --       auto_refresh = false,
+  --       keymap = {
+  --         accept = "<CR>",
+  --         jump_prev = "[[",
+  --         jump_next = "]]",
+  --         refresh = "gr",
+  --         open = "<A-CR>",
+  --       },
+  --     },
+  --     filetypes = {
+  --       markdown = true,
+  --       help = true,
+  --     },
+  --   },
+  -- },
 
-  -- copilot cmp source
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "copilot" then
-              copilot_cmp._on_insert_enter({})
-            end
-          end)
-        end,
-      },
-    },
-  },
+  -- -- copilot cmp source
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = {
+  --     {
+  --       "zbirenbaum/copilot-cmp",
+  --       dependencies = "copilot.lua",
+  --       opts = {},
+  --       config = function(_, opts)
+  --         local copilot_cmp = require("copilot_cmp")
+  --         copilot_cmp.setup(opts)
+  --         require("lazyvim.util").lsp.on_attach(function(client)
+  --           if client.name == "copilot" then
+  --             copilot_cmp._on_insert_enter({})
+  --           end
+  --         end)
+  --       end,
+  --     },
+  --   },
+  -- },
 }
